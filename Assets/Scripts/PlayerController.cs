b21cbeca8a3f1cb37b8b4d3d2860d0afab7c3bce
lucas-machado <lucasmach@gmail.com>
2020-07-03 08:48:44 -0300

    final state class 9

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Rigidbody2D rigidbody2D;
+    public float runBoost = 5;
+    public float jumpBoost = 400;
+
+    float horizontalMove;
+    bool jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalMove < 0)
+        {
+            //raposa olhe para a esquerda
+            spriteRenderer.flipX = true;
+        }
+        else if (horizontalMove > 0)
+        {
+            //raposa olhe para a direita
+            spriteRenderer.flipX = false;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = new Vector2(horizontalMove * runBoost, rigidbody2D.velocity.y);
+
+        if (jump == true)
+        {
+            rigidbody2D.AddForce(new Vector2(0, jumpBoost));
+            jump = false;
+        }
+    }
+}
