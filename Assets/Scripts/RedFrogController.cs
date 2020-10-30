76aa054c028d93f101968f605dae72c6f689351b
lucas-machado <lucasmach@gmail.com>
2020-10-30 07:05:38 -0300

    preparing to create fireball

---
+++ Assets/Scripts/RedFrogController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedFrogController : MonoBehaviour
+{
+    public GameObject fireball;
+    public float waitToShoot = 2.0f;
+
+    float secondsPassed = 0;
+
+    void Update()
+    {
+        secondsPassed += Time.deltaTime;
+
+        if (secondsPassed >= waitToShoot)
+        {
+            Instantiate(fireball, transform.position, Quaternion.identity);
+            secondsPassed = 0;
+        }
+    }
+}
